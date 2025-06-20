933baddba5ca9cfda40969f72a4b203f2d5c66a5
JorgeLucasPinheiro98 <jorgejdn@hotmail.com>
2025-06-20 15:37:30 -0300

    integração de validator yup

--- src/domain/product/entity/entity.product.spec.ts
+++ src/domain/product/entity/entity.product.spec.ts
@@ -16,7 +16,7 @@ describe("Product unit tests", () => {
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("product: Price must be greater than zero");
+    }).toThrowError("product: price must be greater than or equal to 1");
   });
 
   it("should throw error when id and name is empty", () => {
